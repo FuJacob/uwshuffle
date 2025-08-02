217438f753d02ad85167da82a12447882b8be2b2
Jacob Fu <jjacobfu@gmail.com>
2025-08-02 01:16:28 -0400

    dea

--- uwshuffle/src/components/Sidebar.tsx
+++ uwshuffle/src/components/Sidebar.tsx
@@ -179,7 +179,7 @@ const Sidebar: React.FC = () => {
                       className="uwshuffle-instructions-video"
                       width="560"
                       height="315"
-                      src="https://www.youtube.com/embed/VkiwIn8Dcaw?si=40WtIsuVn1EgUHlA&amp;controls=0&autoplay=1&mute=1"
+                      src="https://www.youtube.com/embed/VkiwIn8Dcaw?si=40WtIsuVn1EgUHlA&amp;controls=0&autoplay=1&mute=1&loop=1"
                       title="YouTube video player"
                       frameBorder="0"
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
